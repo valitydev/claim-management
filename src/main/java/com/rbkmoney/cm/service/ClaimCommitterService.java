f3515f6f11a759ed95d83968e7d9017f1017f0a6
Pavel Popov <tolkonepiu@users.noreply.github.com>
2020-05-29 17:16:11 +0300

    More logs in claim commiter (#24)

--- src/main/java/com/rbkmoney/cm/service/ClaimCommitterService.java
+++ src/main/java/com/rbkmoney/cm/service/ClaimCommitterService.java
@@ -14,8 +14,8 @@ import com.rbkmoney.damsel.claim_management.InvalidChangeset;
 import com.rbkmoney.woody.api.flow.WFlow;
 import com.rbkmoney.woody.thrift.impl.http.THSpawnClientBuilder;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.thrift.TException;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -33,41 +33,36 @@ public class ClaimCommitterService {
     @Transactional
     public void doCommitClaim(String partyId, long claimId, int revision) {
         log.info("Trying to commit and accept claim, partyId='{}', claimId='{}'", partyId, claimId);
-        try {
-            ClaimModel claimModel = claimManagementService.getClaim(partyId, claimId);
+        ClaimModel claimModel = claimManagementService.getClaim(partyId, claimId);
 
-            if (claimModel.getRevision() != revision) {
-                throw new InvalidRevisionException(
-                        String.format("Invalid claim revision, expected='%s', actual='%s'", claimModel.getRevision(), revision)
-                );
-            }
-
-            if (claimModel.getClaimStatus().getClaimStatusEnum() != ClaimStatusEnum.pending_acceptance) {
-                throw new InvalidClaimStatusException(
-                        String.format("Invalid claim status, expected='%s', actual='%s'", ClaimStatusEnum.pending_acceptance, claimModel.getClaimStatus().getClaimStatusEnum()),
-                        claimModel.getClaimStatus()
-                );
-            }
-
-            new WFlow().createServiceFork(
-                    () -> {
-                        try {
-                            addUserInfoFromLastModification(claimModel);
-                            Claim claim = conversionWrapperService.convertClaim(claimModel);
-                            committers.forEach(committer -> sendAccept(partyId, claim, committer));
-                            claimManagementService.acceptClaim(partyId, claimId, revision);
-                            committers.forEach(committer -> sendCommit(partyId, claim, committer));
-                            log.info("Claim have been commited and accepted, partyId='{}', claimId='{}'", partyId, claimId);
-                        } catch (InvalidChangesetException ex) {
-                            log.warn("An invalid changeset occurred while trying to apply the claim, rollback to pending status", ex);
-                            claimManagementService.failClaimAcceptance(partyId, claimId, revision);
-                        }
-                    }
-            ).run();
-
-        } catch (InvalidClaimStatusException | InvalidRevisionException ex) {
-            throw ex;
+        if (claimModel.getRevision() != revision) {
+            throw new InvalidRevisionException(
+                    String.format("Invalid claim revision, expected='%s', actual='%s'", claimModel.getRevision(), revision)
+            );
         }
+
+        if (claimModel.getClaimStatus().getClaimStatusEnum() != ClaimStatusEnum.pending_acceptance) {
+            throw new InvalidClaimStatusException(
+                    String.format("Invalid claim status, expected='%s', actual='%s'", ClaimStatusEnum.pending_acceptance, claimModel.getClaimStatus().getClaimStatusEnum()),
+                    claimModel.getClaimStatus()
+            );
+        }
+
+        new WFlow().createServiceFork(
+                () -> {
+                    try {
+                        addUserInfoFromLastModification(claimModel);
+                        Claim claim = conversionWrapperService.convertClaim(claimModel);
+                        committers.forEach(committer -> sendAccept(partyId, claim, committer));
+                        claimManagementService.acceptClaim(partyId, claimId, revision);
+                        committers.forEach(committer -> sendCommit(partyId, claim, committer));
+                        log.info("Claim have been commited and accepted, partyId='{}', claimId='{}'", partyId, claimId);
+                    } catch (InvalidChangesetException ex) {
+                        log.warn("An invalid changeset occurred while trying to apply the claim, rollback to pending status", ex);
+                        claimManagementService.failClaimAcceptance(partyId, claimId, revision);
+                    }
+                }
+        ).run();
     }
 
     private void addUserInfoFromLastModification(ClaimModel claimModel) {
@@ -75,29 +70,32 @@ public class ClaimCommitterService {
         ContextUtil.addUserInfoToContext(modificationModel.getUserInfo());
     }
 
+    @SneakyThrows
     public void sendAccept(String partyId, Claim claim, CommitterConfig.Committer committer) {
         log.info("Trying to accept claim in service, serviceId='{}', partyId='{}', claimId='{}'", committer.getId(), partyId, claim.getId());
         try {
             buildClaimCommitterClient(committer).accept(partyId, claim);
             log.info("Claim have been accepted in service, serviceId='{}', partyId='{}', claimId='{}'", committer.getId(), partyId, claim.getId());
         } catch (InvalidChangeset ex) {
+            List<ModificationModel> invalidModifications = conversionWrapperService.convertModifications(ex.getInvalidChangeset());
             throw new InvalidChangesetException(
-                    String.format("Failed to accept claim in service, serviceId='%s', partyId='%s', claimId='%d'", committer.getId(), partyId, claim.getId()),
-                    conversionWrapperService.convertModifications(ex.getInvalidChangeset())
+                    String.format("Failed to accept claim in service, serviceId='%s', partyId='%s', claimId='%d', reason='%s', invalidModifications='%s'",
+                            committer.getId(),
+                            partyId,
+                            claim.getId(),
+                            ex.getReason(),
+                            invalidModifications
+                    ),
+                    invalidModifications
             );
-        } catch (TException ex) {
-            throw new RuntimeException(ex);
         }
     }
 
+    @SneakyThrows
     public void sendCommit(String partyId, Claim claim, CommitterConfig.Committer committer) {
         log.info("Trying to commit claim in service, serviceId='{}', partyId='{}' claimId='{}'", committer.getId(), partyId, claim.getId());
-        try {
-            buildClaimCommitterClient(committer).commit(partyId, claim);
-            log.info("Claim have been commited in service, serviceId='{}', partyId='{}', claimId='{}'", committer.getId(), partyId, claim.getId());
-        } catch (TException ex) {
-            throw new RuntimeException(ex);
-        }
+        buildClaimCommitterClient(committer).commit(partyId, claim);
+        log.info("Claim have been commited in service, serviceId='{}', partyId='{}', claimId='{}'", committer.getId(), partyId, claim.getId());
     }
 
     private ClaimCommitterSrv.Iface buildClaimCommitterClient(CommitterConfig.Committer committer) {
