22342f7a005f027db241e5cec24c34b6b29d56c6
Kostya <koztyastruga@gmail.com>
2021-05-18 18:44:02 +0300

    Revert style in sql (#35)

--- src/main/resources/db/migration/V3__claim_modify_delete.sql
+++ src/main/resources/db/migration/V3__claim_modify_delete.sql
@@ -1,6 +1,3 @@
-ALTER TABLE cm.modification_model
-    ADD COLUMN deleted BOOLEAN DEFAULT false NOT NULL;
-ALTER TABLE cm.modification_model
-    ADD COLUMN removed_at TIMESTAMP WITHOUT TIME ZONE;
-ALTER TABLE cm.modification_model
-    ADD COLUMN changed_at TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE cm.modification_model ADD COLUMN deleted BOOLEAN DEFAULT false NOT NULL;
+ALTER TABLE cm.modification_model ADD COLUMN removed_at TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE cm.modification_model ADD COLUMN changed_at TIMESTAMP WITHOUT TIME ZONE;
