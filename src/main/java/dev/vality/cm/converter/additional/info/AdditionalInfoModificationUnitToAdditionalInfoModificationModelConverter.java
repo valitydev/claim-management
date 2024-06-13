feb303f8d0bf4d53210b14ec731f547cfd3e8f2a
kostyastruga <koztyastruga@gmail.com>
2024-06-13 18:24:39 +0300

    Add handler for party additional info

--- src/main/java/dev/vality/cm/converter/additional/info/AdditionalInfoModificationUnitToAdditionalInfoModificationModelConverter.java
+++ src/main/java/dev/vality/cm/converter/additional/info/AdditionalInfoModificationUnitToAdditionalInfoModificationModelConverter.java
@@ -3,13 +3,10 @@ package dev.vality.cm.converter.additional.info;
 import dev.vality.cm.converter.ClaimConverter;
 import dev.vality.cm.model.AdditionalInfoModificationModel;
 import dev.vality.damsel.claim_management.AdditionalInfoModificationUnit;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
-import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class AdditionalInfoModificationUnitToWalletModificationModelConverter
+public class AdditionalInfoModificationUnitToAdditionalInfoModificationModelConverter
         implements ClaimConverter<AdditionalInfoModificationUnit, AdditionalInfoModificationModel> {
 
     @Override
