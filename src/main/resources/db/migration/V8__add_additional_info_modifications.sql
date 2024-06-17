7094cda17fd1c6215f2fd1916d62d4fc85399339
kostyastruga <koztyastruga@gmail.com>
2024-06-17 14:37:03 +0300

    Add model

--- src/main/resources/db/migration/V8__add_additional_info_modifications.sql
+++ src/main/resources/db/migration/V8__add_additional_info_modifications.sql
@@ -1,8 +1,8 @@
 create table cm.additional_info_modification_model
 (
-    id              int8 not null,
-    party_name      character varying,
-    comment         text,
+    id                          int8 not null,
+    party_name                  character varying,
+    comment                     text,
     manager_contact_emails      text[],
     primary key (id)
 );
